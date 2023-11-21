cb2bb0921fb155ff00e7c6a1324261d201dfae0c
marcocentonze <marco.centonze27@gmail.com>
2023-11-21 15:45:12 +0100

    add SinglePageView route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// route tutti i progetti
 Route::get('projects', function () {
     $projects = Project::with('type', 'technologies')->OrderbyDesc('id')->paginate(9);
     return response()->json([
@@ -27,10 +28,30 @@ Route::get('projects', function () {
     ]);
 });
 
+
+// route ultimi 3 progetti
 Route::get('projects/latest', function () {
     $projects = Project::with('type', 'technologies')->OrderbyDesc('id')->take(3)->get();
     return response()->json([
         'status' => 'success',
         'result' => $projects
     ]);
+});
+
+// route singolo progetto
+Route::get('projects/{project:slug}', function ($slug) {
+
+    $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+    if ($project) {
+        return response()->json([
+            'success' => true,
+            'result' => $project
+        ]);
+    } else {
+        return response()->json([
+            'success' => false,
+            'result' => 'Project not found'
+        ]);
+    }
 });
