3921de415665ecf77ccd0273150d3b409b9feae3
marcocentonze <marco.centonze27@gmail.com>
2023-12-03 18:40:15 +0100

    delete welcome guest page

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -8,11 +8,25 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function projects()
+    public function projects(Request $request)
     {
+        $query = Project::with(['type', 'technologies']);
+
+        // Verifica se è stato fornito un parametro di query 'technology' e non è 'all'
+        if ($request->has('technology') && $request->technology != 'all') {
+            // Aggiungi una condizione alla query per filtrare i progetti in base alla tecnologia
+            $query->whereHas('technologies', function ($q) use ($request) {
+                // Assicurati che il campo utilizzato qui corrisponda a quello nel tuo database
+                $q->where('name', $request->technology);
+            });
+        }
+
+        // Ottieni i progetti filtrati (se applicabile) e paginati
+        $projects = $query->orderByDesc('id')->paginate(9);
+
         return response()->json([
             'status' => 'success',
-            'projects' => Project::with(['type', 'technologies'])->orderByDesc('id')->paginate(9)
+            'projects' => $projects
         ]);
     }
 
